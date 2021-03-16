bf818326453564f27a56aa4514b00507ec9237bc
Zeki Unyildiz <zekiunyildiz@gmail.com>
2021-03-16 12:48:46 +0300

    2.Api hazırladım

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import {useState, useEffect} from "react";
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
 
-  const [users, setUser] = useState([])
+  useEffect(() => {
+    (async () => {
+      let userData;
+      try {
+        const response = await fetch("https://randomuser.me/api/?results=25");
+        const userData = await response.json().results;
+      } catch (error) {
+        console.log(error);
+        userData = [];
+      }
 
-  useEffect(async()=>{
+      setUsers(userData);
+    })();
+  },[]);
 
-  });
-
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
